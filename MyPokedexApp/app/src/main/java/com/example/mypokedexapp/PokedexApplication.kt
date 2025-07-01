b0fe6e95e3231280aafb8fa551764c7e4e9f093c
rrdtlsh <144782829+rrdtlsh@users.noreply.github.com>
2025-07-01 13:51:31 +0700

    Delete MyPokedexApp directory

--- MyPokedexApp/app/src/main/java/com/example/mypokedexapp/PokedexApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mypokedexapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class PokedexApplication : Application()
--- MyPokedexApp/app/src/main/java/com/example/mypokedexapp/domain/model/Pokemon.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mypokedexapp.domain.model
-
-data class Pokemon(
-    val name: String,
-    val imageUrl: String,
-    val isFavorite: Boolean = false // <-- PERBAIKAN: Tambahkan ini
-)
